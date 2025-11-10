175609a2630a5df8849bb1440a7d8e3c73ee92ac
manisha2309 <manisha0731.be23@chitkara.edu.in>
2025-11-11 00:23:54 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://job-board-frontend.netlify.app/',
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
